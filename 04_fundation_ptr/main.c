b4b1ebbfd5973938020cf5f9c71434de583feaa0
shiwei.xing <2263656843@qq.com>
2026-05-24 16:46:43 +0800

    [添加] 通过函数指针取出一个数值

--- 04_fundation_ptr/main.c
+++ 04_fundation_ptr/main.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
-/*
-*bref:结构体有函数指针
-*/
 struct Handler {
     int (*op)(int, int);
     void (*print)(int);
+    void (*get_data)(int *);
 };
 
 
-int add(int a, int b) {
+int value = 42;
+
+// Function declarations
+static  int add(int a, int b);
+static void print_result(int value);
+
+
+
+
+
+
+static  int add(int a, int b) {
     return a + b;
 }
 
-void print_result(int value) {
+static void print_result(int value) {
     printf("result = %d\n", value);
 }
 
 
-/*
-*bref:获取函数地址
-*op:指向了add函数地址
-*print:指向了print_result函数地址
-*return:返回了一个Handler结构体的地址(这样就拿到了函数的地址)
-*/
+void get_data(int *data) {
+    *data = value;
+    printf("get_data: data = %d\n", *data);
+}
+
+
 struct Handler* get_handler(void) {
     static struct Handler handler = {
         .op = add,
         .print = print_result,
+        .get_data = get_data
     };
     return &handler;
 }
--- 04_fundation_ptr/other.c
+++ 04_fundation_ptr/other.c
@@ -8,6 +8,7 @@
 struct Handler {
     int (*op)(int, int);
     void (*print)(int);
+    void (*get_data)(int*);
 };
 
 struct Handler* get_handler(void);
@@ -23,7 +24,9 @@ int main(void) {
     struct Handler *handler = get_handler();
     int result = handler->op(10, 5);
     handler->print(result);
-
+    int data;
+    handler->get_data(&data);
+    printf("main: data = %d\n", data);
     //printf("op addr = %p, print addr = %p\n", (void *)handler->op, (void *)handler->print);
     return 0;
 }
